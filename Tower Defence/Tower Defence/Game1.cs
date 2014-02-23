ba4656eb5b29b258fb79cdce154e83e2ce1139ae
Rohit <rohit.shinde12194@gmail.com>
2014-02-23 07:53:57 +0530

    Added new Enemies
    New enemies have been added.
    The Game1.cs class has been modified so that instead of a single
    texture, it accepts an array of texture. It is passed to the WaveManager
    class whose constructor has been modified to accept an array of 2d
    textures instead of a single texture.
    Every other wave is a different enemy.
    The stats have to still be modified depending on the type of enemy.
    A further improvement is having both types of enemies come
    simultaneously in a wave.

--- Tower Defence/Tower Defence/Game1.cs
+++ Tower Defence/Tower Defence/Game1.cs
@@ -36,6 +36,7 @@ namespace Tower_Defence
         Menu menu;
         Input input;
         public static GameStates gameStates;
+        Texture2D[] enemyTexture = new Texture2D[2];
 
         public Game1()
         {
@@ -46,7 +47,7 @@ namespace Tower_Defence
             graphics.PreferredBackBufferHeight = (level.Height + 1) * 32;
             IsMouseVisible = true;
             Content.RootDirectory = "Content";
-            gameStates=GameStates.Menu;
+            gameStates=GameStates.Running;
         }
 
         protected override void Initialize()
@@ -64,7 +65,6 @@ namespace Tower_Defence
 
             level.AddTexture(grass);
             level.AddTexture(path);
-            Texture2D enemyTexture = Content.Load<Texture2D>("enemy");
 
             arial = Content.Load<SpriteFont>("Arial");
             /*enemy1 = new Enemy(enemyTexture, Vector2.Zero, 100, 10, 0.5f);
@@ -75,6 +75,8 @@ namespace Tower_Defence
             tower = new Tower(towerTexture, Vector2.Zero);
             player = new Player(level, towerTexture);*/
 
+            enemyTexture[0] = Content.Load<Texture2D>("enemy");
+            enemyTexture[1] = Content.Load<Texture2D>("enemy2");
             
             Texture2D towerTexture = Content.Load<Texture2D>("tower");
             Texture2D bulletTexture = Content.Load<Texture2D>("bullet");
--- Tower Defence/Tower Defence/WaveManager.cs
+++ Tower Defence/Tower Defence/WaveManager.cs
@@ -12,7 +12,7 @@ namespace Tower_Defence
         private int numberOfWaves;
         private float timeSinceLastWave;
         private Queue<Wave> waves = new Queue<Wave>();
-        private Texture2D enemyTexture;
+        private Texture2D[] enemyTexture;
         private bool waveFinished=false;
         private Level level;
 
@@ -31,7 +31,7 @@ namespace Tower_Defence
             get { return CurrentWave.RoundNumber + 1; }
         }
 
-        public WaveManager(Player player, Level level, int numberOfWaves, Texture2D enemyTexture)
+        public WaveManager(Player player, Level level, int numberOfWaves, Texture2D[] enemyTexture)
         {
             this.numberOfWaves = numberOfWaves;
             this.enemyTexture = enemyTexture;
@@ -41,7 +41,7 @@ namespace Tower_Defence
             {
                 int initialNumberOfEnemies = 6;
                 int numberModifier = i + 1;
-                Wave wave = new Wave(i, initialNumberOfEnemies * numberModifier,player, level, enemyTexture);
+                Wave wave = new Wave(i, initialNumberOfEnemies * numberModifier, player, level, enemyTexture[i % 2]);
                 waves.Enqueue(wave);
             }
 
